4031acc0cab74912420d1bbe672e68b03d4becbc
dybev <54762882+DyBev@users.noreply.github.com>
2026-07-03 18:37:04 +0100

    chore(post): added a post request

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
+
+type Name struct {
+	Name string `json:"name"`
+}
 
 func main() {
 	serveHello := func(w http.ResponseWriter, _ *http.Request) {
@@ -17,8 +23,28 @@ func main() {
 		io.WriteString(w, "bye world")
 	}
 
+	serveName := func(w http.ResponseWriter, req *http.Request) {
+		body, err := io.ReadAll(req.Body)
+		if err != nil {
+			log.Printf("error getting the body data %v", err)
+			io.WriteString(w, "error getting body data")
+			return
+		}
+		var bodyData Name
+		if err := json.Unmarshal(body, &bodyData); err != nil {
+			log.Printf("error unmarshalling the body data %v", err)
+			io.WriteString(w, "error unmarshalling body data")
+			return
+		}
+
+		log.Printf("request at POST /name body %s", bodyData.Name)
+		io.WriteString(w, fmt.Sprintf("hello %s", bodyData.Name))
+	}
+
+
 	http.HandleFunc("/hello", serveHello)
 	http.HandleFunc("/bye", serveBye)
+	http.HandleFunc("POST /name", serveName)
 
 	log.Print("listening on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
